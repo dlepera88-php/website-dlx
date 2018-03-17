ca2c81504d274aaf230dcc67d1eb1198d7f92c1f
dlepera <dlepera88@gmail.com>
2018-03-16 21:07:52 -0300

    Inclusão dos módulos referentes a Informações de contato na query de criação do banco de dados.

--- bd/website-dlx-dados.sql
+++ bd/website-dlx-dados.sql
@@ -4,10 +4,12 @@ INSERT INTO dlx_paineldlx_modulos (modulo_nome, modulo_descr, modulo_ordem, modu
 
 SET @ID_MODULO = last_insert_id();
 
-INSERT INTO dlx_paineldlx_modulos (modulo_pai, modulo_nome, modulo_descr, modulo_link) VALUES
-    (@ID_MODULO, 'Configurações', 'Gerenciar configurações do website.', 'website/configuracoes'),
-    (@ID_MODULO, 'Assuntos de contatos', 'Assuntos a serem exibidos no formulário de contato para facilitar o tratamentos dos contatos recebidos.', 'website/assuntos-de-contatos'),
-    (@ID_MODULO, 'Contatos recebidos', 'Contatos recebidos através do formulário do website.', 'website/contatos-recebidos');
+INSERT INTO dlx_paineldlx_modulos (modulo_pai, modulo_nome, modulo_descr, modulo_link, modulo_exibir_menu) VALUES
+    (@ID_MODULO, 'Configurações', 'Gerenciar configurações do website.', 'website/configuracoes', 1),
+    (@ID_MODULO, 'Assuntos de contatos', 'Assuntos a serem exibidos no formulário de contato para facilitar o tratamentos dos contatos recebidos.', 'website/assuntos-de-contatos', 1),
+    (@ID_MODULO, 'Contatos recebidos', 'Contatos recebidos através do formulário do website.', 'website/contatos-recebidos', 1),
+    (@ID_MODULO, 'Tipos de informações de contato', 'Tipos de informações de contatos aceitas. Essa configuração pode incluir validação e máscara de dados.', 'website/tipos-de-informacoes', 0),
+    (@ID_MODULO, 'Informações de contato', 'Informações de contato e redes sociais.', 'website/informacoes-de-contato', 1);
 
 -- CRIAR REGISTRO DE CONFIGURAÇÕES DO WEBSITE
 INSERT INTO dlx_websitedlx_configuracoes VALUES ('padrao');
