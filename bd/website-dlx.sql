bd959f8d72622d98fbee28ba3044043cbb5fdfd0
dlepera <dlepera88@gmail.com>
2019-08-18 15:19:34 -0300

    Correção do arquivo SQL.

--- bd/website-dlx.sql
+++ bd/website-dlx.sql
@@ -29,15 +29,17 @@ CREATE TABLE dlx_contatos_recebidos (
 ) ENGINE = INNODB;
 
 -- INFORMAÇÕES DE CONTATO
+DROP TABLE IF EXISTS dlx_informacao_contato_tipo;
 CREATE TABLE dlx_informacao_contato_tipo (
-    informcao_contato_tipo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    informacao_contato_tipo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     deletado BOOL NOT NULL DEFAULT 0
 ) ENGINE = INNODB;
 
+DROP TABLE IF EXISTS dlx_informacao_contato;
 CREATE TABLE dlx_informacao_contato (
     informacao_contato_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tipo INT NOT NULL references informcao_contato_tipo_id (informcao_contato_tipo_id),
+    tipo INT NOT NULL references dlx_informacao_contato_tipo (informacao_contato_tipo_id),
     contato VARCHAR(255) NOT NULL,
     deletado BOOL NOT NULL DEFAULT 0
 ) ENGINE=INNODB;
